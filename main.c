88739dcbef2c17fa8136e968b79bf73fbb60a1e6
Voreaux Theo <voreauxtheo@gmail.com>
2025-10-10 17:01:14 +0200

    Messing with TTF library

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
     
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Erreur SDL_Init: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if (TTF_Init() == -1) {
+        printf("Erreur TTF_Init: %s\n", TTF_GetError());
+        SDL_Quit();
         return 1;
     }
 
@@ -19,24 +26,84 @@ int main(int argc, char* argv[]) {
 
     if (!window) {
         printf("Erreur SDL_CreateWindow: %s\n", SDL_GetError());
+        TTF_Quit();
         SDL_Quit();
         return 1;
     }
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Erreur SDL_CreateRenderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    TTF_Font* customFont = TTF_OpenFont("./Fonts/I_HATE_COMIC_SANS.TTF", 4);
+    int resolution = 4;
+    int timer = 0;
+    if (!customFont) {
+        printf("Erreur TTF_OpenFont: %s\n", TTF_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Color White = {255, 255, 255};
     int quit = 0;
     SDL_Event event;
+
     while (!quit) {
+        SDL_Surface* surfaceMessage = TTF_RenderText_Solid(customFont, "Hello World!", White); 
+        if (!surfaceMessage) {
+            printf("Erreur TTF_RenderText_Solid: %s\n", TTF_GetError());
+            break;
+        }
+
+        SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+        SDL_FreeSurface(surfaceMessage);  // ✅ Free the surface
+
+        if (!Message) {
+            printf("Erreur SDL_CreateTextureFromSurface: %s\n", SDL_GetError());
+            break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // clear screen
+        SDL_RenderClear(renderer);
+
+        SDL_Rect Message_rect = {0, 0, 600, 150}; // Adjust width/height
+        SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+        SDL_RenderPresent(renderer);
+
+        SDL_DestroyTexture(Message);  // ✅ Destroy texture
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = 1; 
             }
         }
-        SDL_Delay(16); 
+        if (resolution < 256){
+            if(timer == 6){
+                resolution++;
+                customFont = TTF_OpenFont("./Fonts/I_HATE_COMIC_SANS.TTF", resolution);
+                timer = 0;
+            }
+            else{
+                timer++;
+            }
+        }
+        SDL_Delay(16);
     }
 
-    SDL_DestroyWindow(window);
+    // Cleanup
+    TTF_CloseFont(customFont);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);  // ✅ Only once
+    TTF_Quit();
     SDL_Quit();
 
     return 0;
 }
- 
